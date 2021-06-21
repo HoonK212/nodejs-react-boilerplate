1f76e7ad7f1414b1757a8773a9c79b436d798623
HoonK <b0aeiou@gmail.com>
2021-06-21 20:19:14 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser")
+
+const config = require("./config/key")
+
+const {User} = require("./models/User")
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+// -> app.use(express.urlencoded({extended: true}))
+
+// application/json
+app.use(bodyParser.json());
+// -> app.use(express.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dbUser:dbUserPassword@nodejs-react-boilerplat.kkbqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
