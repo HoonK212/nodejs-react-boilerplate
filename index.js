d84aa6a98e8f3accf4802eb7d282354d5b85c499
HoonK <b0aeiou@gmail.com>
2021-06-21 19:23:09 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://dbUser:dbUserPassword@nodejs-react-boilerplat.kkbqs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
